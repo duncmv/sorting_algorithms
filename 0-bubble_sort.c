2f22107c7499f36c8950e352741a763f6a55d2bc
ayaelas <assalharras@gmail.com>
2023-12-13 09:41:57 -0800

    bubble sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int temp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		temp = 0;
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = arrray[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+			}
+		}
+		i++;
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+In computer science, a sorting algorithm is an algorithm that puts elements of a list into an order.
+Here are some Sorting Algorithms:
+Bubble Sort
+Insertion Sort
+Selection Sort
+Quick Sort
+Shell Sort with Knuth Sequence
+Cocktail Shaker Sort
+Counting Sort
+Merge Sort
+Heap Sort
